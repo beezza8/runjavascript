04cda0f9113bd50979a6499a174696c71637fc1f
beezza <beelovers888@gmail.com>
2022-01-01 13:51:43 +0900

    committed on 1/1/2022

---
+++ README.md
@@ -0,0 +1,20 @@
+# RunJavaScript -run javascript code in your code!
+## How to install
+npm:
+```shell
+$ npm install runjavascript
+```
+yarn:
+```shell
+$ yarn add runjavascript
+```
+## Example
+javascript
+```javascript
+const runjavascript = require("runjavascript");
+
+//run javascript code
+console.log(runjavascript.run('console.log("hello!")'));
+
+//resualt : hello!
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+const chp = require("child_process")
+const fs = require("fs")
+
+
+module.exports = {
+    run  : function (JavaScript, ...option) {
+        if (JavaScript instanceof Array || JavaScript instanceof Object) throw Error("JavaScriptCode must be String")
+        fs.writeFileSync("runjs.temp", JavaScript)
+        const out = chp.execSync("node runjs.temp")
+        if (option[0] === true) {
+            return out.toString()
+        }else {
+            fs.unlinkSync("runjs.temp")
+            return out.toString()
+        }
+    }
+}
